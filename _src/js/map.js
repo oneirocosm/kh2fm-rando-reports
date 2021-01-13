894c6019be610375d345fc630572f8d2e5f5b2d6
Colton Crowe <coltontcrowe@gmail.com>
2021-01-13 01:57:22 -0800

    ci: add javascript linting
    lints javascript using eslint and prettier
    automatically run the lint prior to a commit using lint-staged

--- _src/js/map.js
+++ _src/js/map.js
@@ -1,60 +1,68 @@
-// worldName is imported from HTML
+/* global L */
+/* global worldName */
 
 class State {
-    constructor() {
-        this.markers = [];
-        this.selected = undefined;
-    }
+  constructor() {
+    this.markers = [];
+    this.selected = undefined;
+  }
 }
 
-var state = new State();
+const state = new State();
 
-let processChest = function(chest) {
-    let marker;
-    // a debug mode to make develpment easier
-    if (chest.mapLat == null || chest.mapLon == null) {
-        console.log("debug");
-        marker = L.marker([0.0, 0.0], {
-            draggable: true
-        }).addTo(map)
-            .bindPopup(chestPopup(chest));
+const processChest = function (chest) {
+  let marker;
+  // a debug mode to make develpment easier
+  if (chest.mapLat == null || chest.mapLon == null) {
+    console.log("debug");
+    marker = L.marker([0.0, 0.0], {
+      draggable: true,
+    })
+      .addTo(map)
+      .bindPopup(chestPopup(chest));
 
-        marker.on("dragend", function(e) {
-            var marker = e.target;
-            var position = marker.getLatLng();
-            console.log(position.lat.toString() + " : " + position.lng.toString());
-        });
-    } else {
-        marker = L.marker([chest.mapLat, chest.mapLon]).addTo(map)
-            .bindPopup(chestPopup(chest));
-    }
-    return marker;
+    marker.on("dragend", function (e) {
+      const marker = e.target;
+      const position = marker.getLatLng();
+      console.log(position.lat.toString() + " : " + position.lng.toString());
+    });
+  } else {
+    marker = L.marker([chest.mapLat, chest.mapLon])
+      .addTo(map)
+      .bindPopup(chestPopup(chest));
+  }
+  return marker;
 };
 
-let chestPopup = function(chest) {
-    return `Journal World: ${chest.journalWorld}<br>` +
+const chestPopup = function (chest) {
+  return (
+    `Journal World: ${chest.journalWorld}<br>` +
     `Journal Number: ${chest.journalId}<br>` +
-    `ID: ${chest.gameId}`;
-}
+    `ID: ${chest.gameId}`
+  );
+};
 
-let map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -1,
-    maxZoom: 4,
-    zoomSnap: 0.25
+const map = L.map("map", {
+  crs: L.CRS.Simple,
+  minZoom: -1,
+  maxZoom: 4,
+  zoomSnap: 0.25,
 });
 
-var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay(`/img/${worldName}.png`, bounds).addTo(map);
+const bounds = [
+  [0, 0],
+  [1000, 1000],
+];
+L.imageOverlay(`/img/${worldName}.png`, bounds).addTo(map);
 
 fetch("/js/chests.json")
-    .then(response => response.json())
-    .then(worlds => {
-        for (chest of worlds[worldName]) {
-            state.selected = "this works";
-            let marker = processChest(chest);
-            state.markers.push(marker);
-        }
-    });
+  .then((response) => response.json())
+  .then((worlds) => {
+    for (const chest of worlds[worldName]) {
+      state.selected = "this works";
+      const marker = processChest(chest);
+      state.markers.push(marker);
+    }
+  });
 
 map.fitBounds(bounds);
